f324e0dc40e703f01fa8b04033d9702aca82ba12
James Stephenson <c4p7.fl1n7@gmail.com>
2020-03-28 18:22:35 -0400

    can now get mainline for pgn games

--- scripts/pgn.js
+++ scripts/pgn.js
@@ -85,5 +85,62 @@ function tokenifyPGN(pgnstr) {
 }
 
 function processPGN(pgnstr) {
-  return tokenifyPGN(stripPGNcomments(pgnstr));
+  return buildGameObjects(tokenifyPGN(stripPGNcomments(pgnstr)));
 }
+
+function buildGameObjects(pgnTokens) {
+  let go = {};
+  go.moveTextTokens = []
+  let i = 0;
+  let tagSection = true;
+  let gameObjects = [];
+
+  function parseMainLine(g) {
+    const moveRe = /.*[a-h][1-8].*/;
+    let mainline = [];
+    let RAVlevel = 0;
+    for (let i=1; i<g.moveTextTokens.length; i++) {
+      let t = g.moveTextTokens[i];
+      if (RAVlevel) {
+        if (t === ")") {
+          RAVlevel--;
+        }
+      } else {
+        if (t === "(") {
+          RAVlevel++;
+        } else {
+          if (moveRe.test(t)) {
+            mainline.push(t);
+          }
+        }
+      }
+    }
+    g["mainline"] = mainline;
+  }
+
+  while (i < pgnTokens.length) {
+    if (tagSection) {
+      if (pgnTokens[i] === "[") {
+        go[pgnTokens[i+1]] = pgnTokens[i+2];
+        i += 4;
+      } else {
+        tagSection = false;
+      }
+    } else {
+      go.moveTextTokens.push(pgnTokens[i]);
+      if (pgnTokens[i++] === go.Result.slice(1,-1)) {
+        parseMainLine(go);
+        gameObjects.push(go);
+        go = {};
+        go.moveTextTokens = [];
+      }
+    }
+  }
+  return gameObjects;
+}
+
+var test = document.getElementById("testpgn")
+test = test.textContent;
+test = processPGN(test);
+test = test[0];
+console.log(test.mainline);
